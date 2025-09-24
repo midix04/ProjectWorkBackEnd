db8269699f3fed88ea4c84f9bd38111b77fca6c3
MichelePar1 <michele.parisi@allievi.itsdigitalacademy.com>
2025-09-24 10:20:37 +0200

    Changes for deploy

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,21 @@ import 'reflect-metadata';
 import { createServer } from 'http';
 import app from './app';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost:27017/ProjectWork')
-    .then(_ => {
-        createServer(app).listen(3000, () => {
-            console.log('Server listening on port 3000');
-        });
-    })
-    .catch(err => {
-        console.error(err);
-    })
 
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/ProjectWork";
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    createServer(app).listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
